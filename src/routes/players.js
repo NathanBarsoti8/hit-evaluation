bb23a51002bb6cfd4460e1677de9a58c61372808
Nathan Barsoti <nathanbarsoti@hotmail.com>
2022-04-18 23:28:12 -0300

    adjust to swagger

--- src/routes/players.js
+++ src/routes/players.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.post('', require('../../src/services/players/create'))
+router.post('/', require('../../src/services/players/create'))
 router.put('/:id', require('../../src/services/players/update'))
 
 module.exports = router
